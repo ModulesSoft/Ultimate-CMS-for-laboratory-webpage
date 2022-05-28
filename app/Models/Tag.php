d135b0ea87b412417f15538bd22ed3fb763b9bad
Yousef Fatouraee <fatouraee96@gmail.com>
2022-05-28 14:39:46 +0430

    removed slugs for tags

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,14 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Backpack\CRUD\app\Models\Traits\SpatieTranslatable\Sluggable;
-use Backpack\CRUD\app\Models\Traits\SpatieTranslatable\SluggableScopeHelpers;
 use Backpack\CRUD\app\Models\Traits\SpatieTranslatable\HasTranslations;
 
 class Tag extends Model
 {
     use \Backpack\CRUD\app\Models\Traits\CrudTrait;
-    use Sluggable, SluggableScopeHelpers;
     use HasTranslations;
 
     protected $table = 'tags';
@@ -25,37 +22,14 @@ class Tag extends Model
 
     public function articles()
     {
-        return $this->belongsToMany(Article::class, 'article_tag');
+        return $this->belongsToMany(Article::class);
     }
     public function profiles()
     {
-        return $this->belongsToMany(Profile::class, 'profile_tag');
+        return $this->belongsToMany(Profile::class);
     }
     public function sections()
     {
-        return $this->belongsToMany(Section::class, 'section_tag');
-    }
-
-    /**
-     * Return the sluggable configuration array for this model.
-     *
-     * @return array
-     */
-    public function sluggable()
-    {
-        return [
-            'slug' => [
-                'source' => 'slug_or_name',
-            ],
-        ];
-    }
-    // The slug is created automatically from the "name" field if no slug exists.
-    public function getSlugOrNameAttribute()
-    {
-        if ($this->slug != '') {
-            return $this->slug;
-        }
-
-        return $this->name;
+        return $this->belongsToMany(Section::class);
     }
 }
