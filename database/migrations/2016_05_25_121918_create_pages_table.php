cb54a5c92f85b03538217db502c0a749eaf20db6
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-05-28 14:09:52 +0430

    Modified tables

--- database/migrations/2016_05_25_121918_create_pages_table.php
+++ database/migrations/2016_05_25_121918_create_pages_table.php
@@ -18,14 +18,14 @@ class CreatePagesTable extends Migration
         // TODO: use JSON data type for 'extras' instead of string
         Schema::create('pages', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('template');
+            // $table->string('template')->nullable();
             $table->string('name');
             $table->string('title');
             $table->string('slug');
-            $table->text('content')->nullable();
+            $table->text('introduction')->nullable();
+            $table->text('conclusion')->nullable();
             $table->text('extras')->nullable();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
