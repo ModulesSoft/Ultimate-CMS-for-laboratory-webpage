21deba944aeaacaa70d574c3f5627bde3847bf0f
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-05-30 01:19:01 +0430

    Added icon

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -26,7 +26,8 @@ class CategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name' => 'required|min:5|max:255'
+            'name' => 'required|min:3|max:255',
+            'icon' => 'string|unique:categories,icon',
         ];
     }
 
