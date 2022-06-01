a5e23d7940cc72615e80a63012737217f3a7d050
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-06-01 14:42:58 +0430

    Added profile supervisor migration

---
+++ database/migrations/2022_06_01_095234_create_profile_supervisor_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileSupervisorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile_supervisor', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('profile_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile_supervisor');
+    }
+}
