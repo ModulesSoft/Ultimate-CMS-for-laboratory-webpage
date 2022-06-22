70dc72762014891c4d05370d13a470230f37e065
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-06-22 15:10:32 +0430

    Added APIs

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Spatie\QueryBuilder\QueryBuilder;
+use App\Models\User;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Page;
+use App\Models\Profile;
+use App\Models\Setting;
+use App\Models\Slideshow;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +25,37 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(
+    ['prefix' => "v1"],
+    function () {
+        Route::get('/users', fn () => QueryBuilder::for(User::class)
+            ->allowedFilters(['name', 'email'])
+            ->get());
+        Route::get('/articles', fn () => QueryBuilder::for(Article::class)
+            ->allowedFilters(['title', 'featured', 'category_id'])
+            ->get());
+
+
+        Route::get('/slides', fn () => QueryBuilder::for(Slideshow::class)
+            ->get());
+        Route::get('/pages', fn () => QueryBuilder::for(Page::class)
+            ->get());
+        Route::get('/categories', fn () => QueryBuilder::for(Category::class)
+            ->orderBy('lft')->allowedFilters(['slug'])->get());
+        Route::get('/settings', fn () => QueryBuilder::for(Setting::class)
+            ->allowedFilters(['key'])->get());
+        Route::get('/students', fn () => QueryBuilder::for(
+            User::whereHas('roles', function ($query) {
+                $supervisor_roles = explode(',', env('SUPERVISOR_ROLES'));
+                array_push($supervisor_roles, 'admin');
+                $query->whereNotIn('name', $supervisor_roles)->with('role');
+            })
+        )->allowedFilters(['name'])->get());
+
+        // User::with(['roles' => function ($query) {
+        // $query->with(['role', 'Professor']);
+        // Route::get('/sendMail');
+        // footer
+    }
+);
