3a4de35e74e9ea923d44cd1df7a6f5b74fc46aea
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-06-26 13:49:01 +0430

    Extracted js and css

---
+++ resources/views/prototype/js/index.js
@@ -0,0 +1,36 @@
+
+    // hamburger menu
+    function w3_open() {
+        document.getElementById("main").style.marginLeft = "25%";
+        document.getElementById("mySidebar").style.width = "25%";
+        document.getElementById("mySidebar").style.display = "block";
+        document.getElementById("openNav").style.display = "none";
+    }
+
+    function w3_close() {
+        document.getElementById("main").style.marginLeft = "0%";
+        document.getElementById("mySidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+    }
+    // slideshow
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs((slideIndex += n));
+    }
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("slideshow__image");
+        if (n > x.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = x.length;
+        }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
+    }
