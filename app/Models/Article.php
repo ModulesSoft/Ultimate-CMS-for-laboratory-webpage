ad065119199e8ebbca2c62adc75098bd255262a1
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-06-30 19:45:12 +0430

    Fixed sluggable error

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -52,7 +52,7 @@ class Article extends Model
      *
      * @return array
      */
-    public function sluggable()
+    public function sluggable(): array
     {
         return [
             'slug' => [
