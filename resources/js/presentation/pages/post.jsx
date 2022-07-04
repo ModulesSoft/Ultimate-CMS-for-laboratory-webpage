a886f334219b1ee728cb381f6dee1315f9c5f926
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-07-04 17:54:37 +0430

    Extracted some repetitive code

--- resources/js/presentation/pages/post.jsx
+++ resources/js/presentation/pages/post.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useFetchPost } from "../../infrastructure/APIHandler";
-import Layout from "../layouts/layout";
 import { useParams } from "react-router-dom";
+import Loading from "../components/loading";
+import { Helmet } from "react-helmet";
 export const Post = () => {
     let { slug } = useParams();
     const { data, loading } = useFetchPost({
@@ -9,53 +10,60 @@ export const Post = () => {
     });
 
     const post = data[0];
-    const found = post ? true : false;
     return (
-        found && (
-            <Layout
-                title={post.title.en}
-                header={<></>}
-                keywords=""
-                description=""
-                found={found}
-            >
-                <article className="w3-container">
-                    <section>
-                        <div className="w3-card-4">
-                            <header className="w3-container w3-teal">
-                                <h1 className="w3-center">{post.title.en}</h1>
-                            </header>
-                            <div
-                                className="w3-container w3-padding"
-                                style={{ width: "75%" }}
-                            >
-                                <img
-                                    src={post.image}
-                                    alt={post.slug}
-                                    className="w3-round w3-margin"
-                                />
-                                <p
-                                    className="w3-margin"
-                                    dangerouslySetInnerHTML={{
-                                        __html: post.content.en,
-                                    }}
-                                ></p>
+        <>
+            {loading ? (
+                <Loading />
+            ) : (
+                <>
+                    <Helmet>
+                        <meta name="description" content={""} />
+                        <meta name="keywords" content={""} />
+                        <title>{""}</title>
+                    </Helmet>
+                    {/* <header>
+                        <Header h1={post.title.en} />
+                    </header> */}
+                    <article className="w3-container">
+                        <section>
+                            <div className="w3-card-4">
+                                <header className="w3-container w3-teal">
+                                    <h1 className="w3-center">
+                                        {post.title.en}
+                                    </h1>
+                                </header>
+                                <div
+                                    className="w3-container w3-padding"
+                                    style={{ width: "75%" }}
+                                >
+                                    <img
+                                        src={post.image}
+                                        alt={post.slug}
+                                        className="w3-round w3-margin"
+                                    />
+                                    <p
+                                        className="w3-margin"
+                                        dangerouslySetInnerHTML={{
+                                            __html: post.content.en,
+                                        }}
+                                    ></p>
+                                </div>
+                                <footer className="w3-container w3-light-grey">
+                                    {post.tags.map((tag, index) => (
+                                        <h5
+                                            key={index}
+                                            className="w3-tag w3-teal w3-round"
+                                        >
+                                            {tag}
+                                        </h5>
+                                    ))}
+                                </footer>
                             </div>
-                            <footer className="w3-container w3-light-grey">
-                                {post.tags.map((tag, index) => (
-                                    <h5
-                                        key={index}
-                                        className="w3-tag w3-teal w3-round"
-                                    >
-                                        {tag}
-                                    </h5>
-                                ))}
-                            </footer>
-                        </div>
-                    </section>
-                </article>
-            </Layout>
-        )
+                        </section>
+                    </article>
+                </>
+            )}
+        </>
     );
 };
 export default Post;
