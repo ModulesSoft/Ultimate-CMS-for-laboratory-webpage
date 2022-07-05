6a076239aa8967a2c7bfe99de19eb31887df5db4
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-07-05 18:28:25 +0430

    Added pages routes

--- resources/js/presentation/pages/page.jsx
+++ resources/js/presentation/pages/page.jsx
@@ -1,81 +1,47 @@
 import Header from "../components/header";
-import Layout from "../layouts/layout";
-export const Page = () => (
-    <Layout
-        title="PageA"
-        header={<Header h1="Page" />}
-        keywords="page, articles"
-        description="PageA page"
-    >
-        <section>
-            <article>
-                <div className="w3-row w3-margin">
-                    <header>
-                        <p className="w3-row w3-padding w3-blue-grey">
-                            Introduction Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Accusamus odio fugiat
-                            perspiciatis. Natus dolores libero recusandae saepe
-                            et nostrum aut sequi voluptatum nulla. Nulla, rem
-                            recusandae eaque quasi vitae perspiciatis sequi
-                            reiciendis beatae voluptatem minus unde enim maxime
-                            et dolorum eos adipisci a soluta corrupti
-                            blanditiis. Possimus eveniet ab suscipit sunt!
-                            Voluptate consequatur nesciunt dolore! Nemo ipsum
-                            facere consectetur sunt molestiae eligendi officia
-                            exercitationem quas at, numquam placeat asperiores
-                            modi, perspiciatis explicabo officiis rerum.
-                            Perspiciatis provident quidem tempore accusamus
-                            exercitationem eum architecto iste! Iure, ea nobis
-                            fugiat iste ratione totam magnam est quibusdam?
-                            Natus facilis quasi voluptatum et harum incidunt.
-                        </p>
-                    </header>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis doloremque voluptatem nobis hic quisquam ullam
-                        possimus vel soluta quasi corrupti, magni, voluptatibus
-                        repellendus culpa? Ea itaque temporibus quia voluptatum
-                        unde enim natus deserunt exercitationem cupiditate
-                        laborum. Reiciendis molestias aut sapiente ipsam, fuga
-                        quia ullam assumenda! Dolor nam vitae ullam eos
-                        quibusdam, porro omnis. Maiores dicta nemo provident
-                        magnam. Aspernatur magnam illum, alias harum dolor
-                        voluptates omnis totam praesentium odit, enim eos iusto
-                        accusantium vel dignissimos placeat eaque magni maxime
-                        vitae a assumenda hic et consectetur voluptatem tenetur!
-                        Eius ipsum ea officiis laboriosam animi ipsa harum
-                        dolores? Corrupti nostrum cupiditate unde repellendus
-                        optio sapiente odit ipsum at similique dolorum,
-                        obcaecati placeat neque fugiat! Pariatur dolorum alias
-                        dolorem veritatis soluta ipsam aut, sunt, molestiae
-                        laborum ipsa velit? Natus, officiis nihil, laudantium
-                        aut vitae dolore ea unde modi perspiciatis aliquid
-                        aliquam illum tenetur vel! Ab sapiente asperiores
-                        officiis, minus magni recusandae explicabo dolorem quo
-                        fuga iusto, facere ullam amet temporibus eum nam
-                        provident alias? Odio inventore sed cum mollitia alias
-                        eos dolores, animi consequatur deleniti pariatur soluta
-                        voluptatum nesciunt consequuntur illum adipisci culpa
-                        praesentium necessitatibus dolorum doloremque a
-                        accusamus? Numquam reiciendis natus culpa assumenda ex
-                        ut maiores, placeat perspiciatis error, qui soluta
-                        minima.
-                    </p>
-                    <footer>
-                        <p className="w3-row w3-padding w3-teal">
-                            Conclusion Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Error reiciendis et non, eius unde
-                            nostrum delectus molestiae suscipit temporibus neque
-                            veniam architecto laudantium sit vero eligendi amet
-                            expedita inventore ratione nihil tenetur? Voluptates
-                            laborum ducimus sint explicabo sit quisquam soluta
-                            doloribus, dolores, aliquid, dolore minima tempore
-                            aliquam? Repellat, iure quisquam?
-                        </p>
-                    </footer>
-                </div>
-            </article>
-        </section>
-    </Layout>
-);
+import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
+
+export const Page = ({ title, description, keywords, data }) => {
+    const { i18n } = useTranslation();
+    return (
+        <>
+            <Helmet>
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <title>{title}</title>
+            </Helmet>
+            <header>
+                <Header h1={title} />
+            </header>
+            <section>
+                <article>
+                    <div className="w3-row w3-margin">
+                        <header>
+                            <p
+                                className="w3-row w3-padding w3-blue-grey"
+                                dangerouslySetInnerHTML={{
+                                    __html:
+                                        data.introduction &&
+                                        data.introduction[i18n.language],
+                                }}
+                            ></p>
+                        </header>
+                        <p>...sections....</p>
+                        <footer>
+                            <p
+                                className="w3-row w3-padding w3-teal"
+                                dangerouslySetInnerHTML={{
+                                    __html:
+                                        data.conclusion &&
+                                        data.conclusion[i18n.language],
+                                }}
+                            ></p>
+                        </footer>
+                    </div>
+                </article>
+            </section>
+        </>
+    );
+};
 export default Page;
