54caed7258ab6bddc1f43aae94638cfb4c710f87
Yousef Fatouraee <fatouraee96@gmail.com>
2022-07-14 12:25:43 +0430

    Added image and thumbnail path generator

--- resources/js/application/common.js
+++ resources/js/application/common.js
@@ -65,3 +65,34 @@ function callbackFunc() {
 // ScrollOut({
 //     /* options */
 // });
+
+export const getThumbUri = (size, uri) => {
+    if (uri) {
+        let thumbDir = "";
+        switch (size) {
+            case "150x150":
+                thumbDir = "thumb150x150";
+                break;
+            case "300x300":
+                thumbDir = "thumb300x300";
+                break;
+            default:
+                return console.error(
+                    "Requested thumbnail image size is not valid"
+                );
+        }
+        //Ensure that the provided uri begins with /
+        uri = getImageUri(uri);
+        //Split to array
+        let splited = uri.split("/");
+        //Add the thumbnail path before the last element
+        splited.splice(splited.length - 1, 0, thumbDir);
+        //Rejoin the array elements to string with /
+        return splited.join("/");
+    } else {
+        return;
+    }
+};
+export const getImageUri = (uri) => {
+    return uri.startsWith("/") ? uri : "/" + uri;
+};
