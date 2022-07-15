868eff27716b4e60a046e5b9c6cd4e8772ccc57f
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-07-15 20:22:38 +0430

    Added footer data

--- resources/js/presentation/components/Footer.jsx
+++ resources/js/presentation/components/Footer.jsx
@@ -1,42 +1,52 @@
-const Footer = () => (
-    <footer className="w3-margin-top">
-        <div className="w3-cell w3-mobile-row">
-          <div className="w3-container w3-dark-grey w3-cell w3-mobile">
-            <article>
-              <h2 className="w3-text-light-blue w3-center">Footer Title</h2>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Consectetur ipsa at delectus sed, aspernatur asperiores molestiae
-                nulla maxime laborum nemo.
-              </p>
-            </article>
-          </div>
-          <div className="w3-container w3-black w3-cell w3-mobile">
-            <article>
-              <h2 className="w3-text-teal w3-center">Footer Title</h2>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Consectetur ipsa at delectus sed, aspernatur asperiores molestiae
-                nulla maxime laborum nemo.
-              </p>
-            </article>
-          </div>
-          <div className="w3-container w3-dark-grey w3-cell w3-mobile">
-            <article>
-              <h2 className="w3-text-light-blue w3-center">Footer Title</h2>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Consectetur ipsa at delectus sed, aspernatur asperiores molestiae
-                nulla maxime laborum nemo.
-              </p>
-            </article>
-          </div>
-          <div className="w3-dark-grey w3-row w3-mobile w3-center">
-            <p className="w3-black w3-padding" style={{ margin: 0 }}>
-              All rights reserved for Majhoolsoft © 2022
-            </p>
-          </div>
-        </div>
-      </footer>
-)
+import { useTranslation } from "react-i18next";
+import { useFetchFooters } from "../../infrastructure/APIHandler";
+import Loading from "./Loading";
+
+const Footer = () => {
+    const { data, loading } = useFetchFooters();
+    const { i18n } = useTranslation();
+    const columns = data;
+    return loading ? (
+        <Loading />
+    ) : (
+        <footer className="w3-margin-top">
+            <div className="w3-cell w3-mobile-row">
+                {columns &&
+                    columns.map((column) => (
+                        <div
+                            key={column.id}
+                            className="w3-container w3-dark-grey w3-cell w3-mobile"
+                        >
+                            <article>
+                                <h2 className="w3-text-light-blue w3-center">
+                                    {column.title[i18n.language]}
+                                </h2>
+                                <ul>
+                                    {column.rows &&
+                                        column.rows.map((row) => (
+                                            <li key={row.id}>
+                                                <a href={row.link}>
+                                                    <img
+                                                        src={row.base64_image}
+                                                        alt=""
+                                                        className="footer__thumbnail"
+                                                    />
+                                                    <span>
+                                                        {
+                                                            row.text[
+                                                                i18n.language
+                                                            ]
+                                                        }
+                                                    </span>
+                                                </a>
+                                            </li>
+                                        ))}
+                                </ul>
+                            </article>
+                        </div>
+                    ))}
+            </div>
+        </footer>
+    );
+};
 export default Footer;
