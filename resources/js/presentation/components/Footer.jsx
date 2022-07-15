ea2f6081c04834e074ee0679788f24104b2fe580
Yousef Fatouraee <fatouraee96@gmail.com>
2022-07-15 22:06:21 +0430

    Fixed footer styles

--- resources/js/presentation/components/Footer.jsx
+++ resources/js/presentation/components/Footer.jsx
@@ -9,60 +9,51 @@ const Footer = () => {
     return loading ? (
         <Loading />
     ) : (
-        <footer className="w3-margin-top">
-            <div className="w3-cell w3-mobile-row">
-                {columns &&
-                    columns.map((column, index) => (
-                        <div
-                            key={column.id}
-                            className={`w3-container ${
-                                index === 1 ? "w3-black" : "w3-dark-grey"
-                            } w3-cell w3-mobile`}
+        <footer className="w3-row w3-margin-top">
+            {columns &&
+                columns.map((column, index) => (
+                    <div
+                        key={column.id}
+                        className={`w3-mobile w3-cell w3-padding ${
+                            index === 1 ? "w3-black" : "w3-dark-grey"
+                        }
+                          `}
+                        style={{ width: (1 / columns.length) * 100 + "%" }}
+                    >
+                        <h2
+                            className={`${
+                                index === 1
+                                    ? "w3-text-teal"
+                                    : "w3-text-light-blue"
+                            } w3-center`}
                         >
-                            <article>
-                                <h2
-                                    className={`${
-                                        index === 1
-                                            ? "w3-text-teal"
-                                            : "w3-text-light-blue"
-                                    } w3-center`}
-                                >
-                                    {column.title[i18n.language]}
-                                </h2>
-                                <ul className="footer__items">
-                                    {column.rows &&
-                                        column.rows.map((row) => (
-                                            <li key={row.id}>
-                                                <a href={row.link}>
-                                                    {row.base64_image && (
-                                                        <img
-                                                            src={
-                                                                row.base64_image
-                                                            }
-                                                            alt=""
-                                                            className={
-                                                                i18n.language ===
-                                                                "en"
-                                                                    ? "footer__thumbnail"
-                                                                    : "footer__thumbnail--rtl"
-                                                            }
-                                                        />
-                                                    )}
-                                                    <span className="w3-text-light-grey">
-                                                        {
-                                                            row.text[
-                                                                i18n.language
-                                                            ]
-                                                        }
-                                                    </span>
-                                                </a>
-                                            </li>
-                                        ))}
-                                </ul>
-                            </article>
-                        </div>
-                    ))}
-            </div>
+                            {column.title[i18n.language]}
+                        </h2>
+                        <ul className="footer__items">
+                            {column.rows &&
+                                column.rows.map((row) => (
+                                    <li key={row.id}>
+                                        <a href={row.link}>
+                                            {row.base64_image && (
+                                                <img
+                                                    src={row.base64_image}
+                                                    alt=""
+                                                    className={
+                                                        i18n.language === "en"
+                                                            ? "footer__thumbnail"
+                                                            : "footer__thumbnail--rtl"
+                                                    }
+                                                />
+                                            )}
+                                            <span className="w3-text-light-grey">
+                                                {row.text[i18n.language]}
+                                            </span>
+                                        </a>
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                ))}
         </footer>
     );
 };
