b8beae5a4bde043de650a045b9ae4f8c482462b5
Yousef Fatouraee <fatouraee96@gmail.com>
2022-07-15 21:00:34 +0430

    Added footer styles

--- resources/js/presentation/components/Footer.jsx
+++ resources/js/presentation/components/Footer.jsx
@@ -12,26 +12,43 @@ const Footer = () => {
         <footer className="w3-margin-top">
             <div className="w3-cell w3-mobile-row">
                 {columns &&
-                    columns.map((column) => (
+                    columns.map((column, index) => (
                         <div
                             key={column.id}
-                            className="w3-container w3-dark-grey w3-cell w3-mobile"
+                            className={`w3-container ${
+                                index === 1 ? "w3-black" : "w3-dark-grey"
+                            } w3-cell w3-mobile`}
                         >
                             <article>
-                                <h2 className="w3-text-light-blue w3-center">
+                                <h2
+                                    className={`${
+                                        index === 1
+                                            ? "w3-text-teal"
+                                            : "w3-text-light-blue"
+                                    } w3-center`}
+                                >
                                     {column.title[i18n.language]}
                                 </h2>
-                                <ul>
+                                <ul className="footer__items">
                                     {column.rows &&
                                         column.rows.map((row) => (
                                             <li key={row.id}>
                                                 <a href={row.link}>
-                                                    <img
-                                                        src={row.base64_image}
-                                                        alt=""
-                                                        className="footer__thumbnail"
-                                                    />
-                                                    <span>
+                                                    {row.base64_image && (
+                                                        <img
+                                                            src={
+                                                                row.base64_image
+                                                            }
+                                                            alt=""
+                                                            className={
+                                                                i18n.language ===
+                                                                "en"
+                                                                    ? "footer__thumbnail"
+                                                                    : "footer__thumbnail--rtl"
+                                                            }
+                                                        />
+                                                    )}
+                                                    <span className="w3-text-light-grey">
                                                         {
                                                             row.text[
                                                                 i18n.language
