ec22de51e851b26438c2b931f7bf564d6b8def9f
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-07-16 22:49:37 +0430

    Extracted page wrapper

--- resources/js/presentation/pages/PostList.jsx
+++ resources/js/presentation/pages/PostList.jsx
@@ -5,6 +5,8 @@ import { Helmet } from "react-helmet";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { getThumbUri } from "../../application/common";
+import PageWrapper from "../components/PageWrapper";
+
 export const posts = ({
     title,
     keywords,
@@ -18,77 +20,70 @@ export const posts = ({
     const posts = data;
     const { t, i18n } = useTranslation();
     return (
-        <>
-            <Helmet>
-                <meta name="description" content={description} />
-                <meta name="keywords" content={keywords} />
-                <title>{title}</title>
-            </Helmet>
+        <PageWrapper
+            description={description}
+            keywords={keywords}
+            title={title}
+            loading={loading}
+        >
             <header>
                 <Header h1={title} />
             </header>
 
-            {loading ? (
-                <Loading />
-            ) : (
-                <article className="w3-container">
-                    {posts.map((post) => (
-                        <section key={post.id}>
-                            <div className="w3-card-4 w3-animate-left">
-                                <header className="w3-container w3-light-grey w3-padding">
-                                    <div className="w3-container w3-cell w3-margin">
-                                        {post.image && (
-                                            <img
-                                                src={getThumbUri(
-                                                    "150x150",
-                                                    post.image
-                                                )}
-                                                alt={post.slug}
-                                                className="w3-round-large posts__thumbnail"
-                                            />
-                                        )}
-                                    </div>
-                                    <div className="w3-container w3-cell w3-margin w3-animate-opacity">
-                                        <h2>{post.title[i18n.language]}</h2>
-                                        <hr />
-                                        <p>
-                                            {t("last update")}
-                                            <time
-                                                className="w3-margin"
-                                                dateTime={post.updated_at}
-                                            >
-                                                {new Date(
-                                                    post.updated_at
-                                                ).toLocaleDateString(
-                                                    i18n.language.replace(
-                                                        "_",
-                                                        "-"
-                                                    )
-                                                )}
-                                            </time>
-                                        </p>
-                                    </div>
-                                </header>
-                                <div className="w3-container w3-padding">
-                                    <p
-                                        className="w3-margin"
-                                        dangerouslySetInnerHTML={{
-                                            __html: post.content[i18n.language],
-                                        }}
-                                    ></p>
+            <article className="w3-container">
+                {posts.map((post) => (
+                    <section key={post.id}>
+                        <div className="w3-card-4 w3-animate-left">
+                            <header className="w3-container w3-light-grey w3-padding">
+                                <div className="w3-container w3-cell w3-margin">
+                                    {post.image && (
+                                        <img
+                                            src={getThumbUri(
+                                                "150x150",
+                                                post.image
+                                            )}
+                                            alt={post.slug}
+                                            className="w3-round-large posts__thumbnail"
+                                        />
+                                    )}
                                 </div>
-                                <Link
-                                    to={`/${categorySlug}/${post.slug}`}
-                                    className="w3-bar w3-button w3-teal"
-                                >
-                                    more
-                                </Link>
+                                <div className="w3-container w3-cell w3-margin w3-animate-opacity">
+                                    <h2>{post.title[i18n.language]}</h2>
+                                    <hr />
+                                    <p>
+                                        {t("last update")}
+                                        <time
+                                            className="w3-margin"
+                                            dateTime={post.updated_at}
+                                        >
+                                            {new Date(
+                                                post.updated_at
+                                            ).toLocaleDateString(
+                                                i18n.language.replace("_", "-")
+                                            )}
+                                        </time>
+                                    </p>
+                                </div>
+                            </header>
+                            <div className="w3-container w3-padding">
+                                <p
+                                    className="w3-margin"
+                                    dangerouslySetInnerHTML={{
+                                        __html: post.content[i18n.language],
+                                    }}
+                                ></p>
                             </div>
-                        </section>
-                    ))}
-                </article>
-            )}
-        </>
+                            <Link
+                                to={`/${categorySlug}/${post.slug}`}
+                                className="w3-bar w3-button w3-teal"
+                            >
+                                more
+                            </Link>
+                        </div>
+                    </section>
+                ))}
+            </article>
+        </PageWrapper>
     );
 };
 
