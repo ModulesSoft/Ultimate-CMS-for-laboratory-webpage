165e217d7a7a1e0444959cf0b5baeae62869bbbf
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-07-20 13:57:25 +0430

    Added tags

--- resources/js/presentation/pages/Profile.jsx
+++ resources/js/presentation/pages/Profile.jsx
@@ -1,22 +1,16 @@
 import PageWrapper from "../components/PageWrapper";
 import { useTranslation } from "react-i18next";
 import { getImageUri } from "../../application/common";
-import { useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import { useFetchProfile } from "../../infrastructure/APIHandler";
 export const Profile = () => {
     const { t, i18n } = useTranslation();
-    const location = useLocation();
-    const loading = false;
-    if (location.state) {
-        // if the data is passed through profile list
-        var { profile } = location.state;
-    } else {
-        const { student } = useParams();
-        console.log(useFetchProfile({ user: student }));
-    }
+    const { student } = useParams();
+    const { data, loading } = useFetchProfile({ user: student });
+    const profile = data;
     return (
         <PageWrapper loading={loading} title="" description="" keywords="">
-            {profile && (
+            {profile.id && (
                 <section>
                     <div className="w3-card-4 w3-center w3-padding w3-mobile">
                         <a href="#">
@@ -62,21 +56,19 @@ export const Profile = () => {
                                 </p>
                                 <hr />
                                 <footer className="w3-container">
-                                    <h5 className="w3-tag w3-teal w3-round">
-                                        keywords
-                                    </h5>
-                                    <h5 className="w3-tag w3-teal w3-round">
-                                        keywords
-                                    </h5>
-                                    <h5 className="w3-tag w3-teal w3-round">
-                                        keywords
-                                    </h5>
-                                    <h5 className="w3-tag w3-teal w3-round">
-                                        keywords
-                                    </h5>
-                                    <h5 className="w3-tag w3-teal w3-round">
-                                        keywords
-                                    </h5>
+                                    {profile.tags.map((tag) => (
+                                        <Link
+                                            key={tag.id}
+                                            to={
+                                                "/tags/" +
+                                                tag.keyword[i18n.language]
+                                            }
+                                        >
+                                            <h5 className="w3-tag w3-teal w3-round">
+                                                {tag.keyword[i18n.language]}
+                                            </h5>
+                                        </Link>
+                                    ))}
                                 </footer>
                             </article>
                         </div>
