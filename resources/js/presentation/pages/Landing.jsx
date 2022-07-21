a33a2818f82d18d6d74f921dc15e44c6a461dff6
Yousef Fatouraee <fatouraee96@gmail.com>
2022-07-21 18:27:41 +0430

    Added landing page style and data

--- resources/js/presentation/pages/Landing.jsx
+++ resources/js/presentation/pages/Landing.jsx
@@ -1,10 +1,21 @@
 import Slideshow from "../components/Slideshow";
 import LandingHeader from "../components/LandingHeader";
 import PageWrapper from "../components/PageWrapper";
-import { useFetchSlides } from "../../infrastructure/APIHandler";
+import { useFetchPosts, useFetchSlides } from "../../infrastructure/APIHandler";
+import { useTranslation } from "react-i18next";
 export const Landing = () => {
-    const { data, loading } = useFetchSlides();
+    let { data, loading } = useFetchSlides();
     const slides = data;
+    ({ data, loading } = useFetchPosts({
+        featured: true,
+    }));
+    const featuredPosts = data;
+    if (featuredPosts[0]) {
+        var featuredPostsFirstRow = featuredPosts.slice(0, 2);
+        var featuredPostsSecondRow = featuredPosts.slice(2, 5);
+    }
+    const { t, i18n } = useTranslation();
+    console.log(featuredPosts);
     return (
         <PageWrapper
             keywords=""
@@ -21,110 +32,69 @@ export const Landing = () => {
                 <div className="featured featured--color-background w3-light-grey">
                     <h2 className="w3-text-dark-grey">
                         <i className="fa-solid fa-star" />
-                        featured 2
+                        <span className="w3-padding">
+                            {t("featuredPostsFirstRow")}
+                        </span>
                     </h2>
                     <div className="w3-cell w3-mobile-row">
-                        <div className="w3-container w3-round-large w3-cell w3-mobile w3-border">
-                            <div className="w3-light-grey w3-padding w3-margin w3-round-large">
-                                <article>
-                                    <h2>Post Title</h2>
-                                    <hr />
-                                    <p className="w3-text-dark-grey">
-                                        Lorem ipsum dolor, sit amet consectetur
-                                        adipisicing elit. Consectetur ipsa at
-                                        delectus sed, aspernatur asperiores
-                                        molestiae nulla maxime laborum nemo.
-                                    </p>
-                                </article>
-                                <button className="w3-button w3-block w3-teal w3-round">
-                                    more
-                                </button>
-                            </div>
-                        </div>
-                        <div className="w3-container w3-round-large w3-cell w3-mobile w3-border">
-                            <div className="w3-light-grey w3-padding w3-margin w3-round-large">
-                                <article>
-                                    <h2>Post Title</h2>
-                                    <hr />
-                                    <p className="w3-text-dark-grey">
-                                        Lorem ipsum dolor, sit amet consectetur
-                                        adipisicing elit. Consectetur ipsa at
-                                        dolorum ducimus tempore laboriosam sunt?
-                                        Similique corrupti aperiam minus
-                                        suscipit quis cumque tenetur aliquid et
-                                        dignissimos.
-                                    </p>
-                                </article>
-                                <button className="w3-button w3-block w3-teal w3-round">
-                                    more
-                                </button>
-                            </div>
-                        </div>
+                        {featuredPostsFirstRow &&
+                            featuredPostsFirstRow.map((post) => (
+                                <div className="w3-container w3-round-large w3-cell w3-mobile w3-border">
+                                    <div className="w3-light-grey w3-padding w3-margin w3-round-large">
+                                        <article>
+                                            <h3>{post.title[i18n.language]}</h3>
+                                            <hr />
+                                            <p
+                                                className="w3-text-dark-grey featured__text"
+                                                dangerouslySetInnerHTML={{
+                                                    __html: post.content[
+                                                        i18n.language
+                                                    ],
+                                                }}
+                                            ></p>
+                                        </article>
+                                        <button className="w3-button w3-block w3-teal w3-round">
+                                            more
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
                     </div>
                 </div>
             </section>
             <section id="featured-3">
                 <div className="featured featured--image-background">
                     <h2 className="w3-text-dark-grey">
-                        <i className="fa-solid fa-star" />
-                        featured 3
+                        <i className="fa-solid fa-bolt" />
+                        <span className="w3-padding">
+                            {t("featuredPostsSecondRow")}
+                        </span>
                     </h2>
                     <div className="w3-cell w3-mobile-row">
-                        <div className="w3-container w3-round-large w3-cell w3-mobile">
-                            <div className="w3-light-grey w3-padding w3-margin w3-round-large">
-                                <article>
-                                    <h2 className="w3-text-dark-grey">
-                                        Post Title
-                                    </h2>
-                                    <hr />
-                                    <p className="w3-text-dark-grey">
-                                        Lorem ipsum dolor, sit amet consectetur
-                                        adipisicing elit. Consectetur ipsa at
-                                        delectus sed, aspernatur asperiores
-                                        molestiae nulla maxime laborum nemo.
-                                    </p>
-                                </article>
-                                <button className="w3-button w3-block w3-teal w3-round">
-                                    more
-                                </button>
-                            </div>
-                        </div>
-                        <div className="w3-container w3-round-large w3-cell w3-mobile">
-                            <div className="w3-light-grey w3-padding w3-margin w3-round-large">
-                                <article>
-                                    <h2>Post Title</h2>
-                                    <hr />
-                                    <p>
-                                        Lorem ipsum dolor, sit amet consectetur
-                                        adipisicing elit. Consectetur ipsa at
-                                        delectus sed, aspernatur asperiores
-                                        molestiae nulla maxime laborum nemo.
-                                    </p>
-                                </article>
-                                <button className="w3-button w3-block w3-teal w3-round">
-                                    more
-                                </button>
-                            </div>
-                        </div>
-                        <div className="w3-container w3-round-large w3-cell w3-mobile">
-                            <div className="w3-light-grey w3-padding w3-margin w3-round-large">
-                                <article>
-                                    <h2 className="w3-text-dark-grey">
-                                        Post Title
-                                    </h2>
-                                    <hr />
-                                    <p className="w3-text-dark-grey">
-                                        Lorem ipsum dolor, sit amet consectetur
-                                        adipisicing elit. Consectetur ipsa at
-                                        delectus sed, aspernatur asperiores
-                                        molestiae nulla maxime laborum nemo.
-                                    </p>
-                                </article>
-                                <button className="w3-button w3-block w3-teal w3-round">
-                                    more
-                                </button>
-                            </div>
-                        </div>
+                        {featuredPostsSecondRow &&
+                            featuredPostsSecondRow.map((post) => (
+                                <div className="w3-container w3-round-large w3-cell w3-mobile">
+                                    <div className="w3-light-grey w3-padding w3-margin w3-round-large">
+                                        <article>
+                                            <h3 className="w3-text-dark-grey">
+                                                {post.title[i18n.language]}
+                                            </h3>
+                                            <hr />
+                                            <p
+                                                className="w3-text-dark-grey featured__text"
+                                                dangerouslySetInnerHTML={{
+                                                    __html: post.content[
+                                                        i18n.language
+                                                    ],
+                                                }}
+                                            ></p>
+                                        </article>
+                                        <button className="w3-button w3-block w3-teal w3-round">
+                                            more
+                                        </button>
+                                    </div>
+                                </div>
+                            ))}
                     </div>
                 </div>
             </section>
@@ -133,8 +103,8 @@ export const Landing = () => {
                     className="w3-text-dark-grey w3-light-blue w3-panel w3-padding"
                     style={{ marginBottom: 0 }}
                 >
-                    <i className="fa-solid fa-star" />
-                    Lorem ipsum dolor sit.
+                    <i className="fa-solid fa-chalkboard-user" />
+                    <span className="w3-padding">{t("professors")}</span>
                 </h2>
                 <div className="w3-cell w3-mobile-row">
                     <div className="w3-cell w3-mobile w3-light-grey w3-card-4 w3-padding w3-round">
@@ -188,8 +158,8 @@ export const Landing = () => {
             <section id="contact">
                 <div className="w3-padding-large w3-light-grey">
                     <h2 className="w3-text-dark-grey">
-                        <i className="fa-solid fa-star" />
-                        Contact us
+                        <i class="fa-solid fa-pen-to-square" />
+                        <span className="w3-padding">{t("contactUs")}</span>
                     </h2>
                     <div className="w3-row w3-center">
                         <div className="w3-third w3-dark-grey w3-padding-24">
