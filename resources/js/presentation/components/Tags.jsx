4e95693a0e40f80a24a84192bde8f4d954ef663f
Yousef Fatouraee <fatouraee96@gmail.com>
2022-07-22 15:27:55 +0430

    Extracted tags component

---
+++ resources/js/presentation/components/Tags.jsx
@@ -0,0 +1,24 @@
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+
+const Tags = ({ tags }) => {
+    const { i18n } = useTranslation();
+    return (
+        tags && (
+            <footer className="w3-container">
+                {tags.map((tag) => (
+                    <Link
+                        className="w3-padding"
+                        key={tag.id}
+                        to={"/tags/" + tag.keyword[i18n.language]}
+                    >
+                        <h5 className="w3-tag w3-teal w3-round">
+                            {tag.keyword[i18n.language]}
+                        </h5>
+                    </Link>
+                ))}
+            </footer>
+        )
+    );
+};
+export default Tags;
