7350a2a6e952551e296f43e9a6a2f184ffdc3b37
Yousef Fatouraee <fatouraee96@gmail.com>
2022-07-23 19:06:31 +0430

    Removed extra tag

--- resources/js/presentation/components/Loading.jsx
+++ resources/js/presentation/components/Loading.jsx
@@ -1,13 +1,11 @@
 export const Loading = () => (
-    <>
-        <div className="w3-container w3-center w3-margin">
-            <i
-                className="fa fa-spinner w3-spin w3-text-teal"
-                style={{ fontSize: "64px" }}
-            ></i>
-            <p>Loading...</p>
-        </div>
-    </>
+    <div className="w3-container w3-center w3-margin">
+        <i
+            className="fa fa-spinner w3-spin w3-text-teal"
+            style={{ fontSize: "64px" }}
+        ></i>
+        {/* <p>Loading...</p> */}
+    </div>
 );
 
 export default Loading;
