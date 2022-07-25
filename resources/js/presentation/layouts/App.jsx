ebe7330708ec7c825f357cb7f3807d0a38e0fa6f
Yousef Fatouraee <fatouraee96@gmail.com>
2022-07-25 12:05:55 +0430

    Replaced CDN links with offline libraries

--- resources/js/presentation/layouts/App.jsx
+++ resources/js/presentation/layouts/App.jsx
@@ -18,17 +18,6 @@ export const Layout = () => (
                     name="viewport"
                     content="width=device-width, initial-scale=1.0"
                 />
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-                    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-                    crossOrigin="anonymous"
-                    referrerPolicy="no-referrer"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://www.w3schools.com/w3css/4/w3.css"
-                />
                 <link href="/css/style.css" rel="stylesheet" />
                 <title>Biofluids Laboratory</title>
             </Helmet>
