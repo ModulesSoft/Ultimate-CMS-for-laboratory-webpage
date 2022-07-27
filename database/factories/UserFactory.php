f8022cefe4249c585da240bb1106e7b2907146f2
majhoolsoft <yousef.fatouraee@aut.ac.ir>
2022-07-27 18:22:59 +0430

    Added database seeder for Super Admin creation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,10 +15,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'id' => 1,
+            'name' => "Super Admin", //$this->faker->name(),
+            'email' => "admin@admin.admin", //$this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt("admin"), //'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
