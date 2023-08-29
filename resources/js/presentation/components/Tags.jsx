b205093c9af0cfbfee9aa4ad54ff49c7cc35921c
Yousef Fatouraee <fatouraee96@gmail.com>
2023-08-29 06:27:05 -0600

    Add title for keywords and change style

--- resources/js/presentation/components/Tags.jsx
+++ resources/js/presentation/components/Tags.jsx
@@ -2,18 +2,19 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 const Tags = ({ tags }) => {
-    const { i18n } = useTranslation();
+    const { i18n,t } = useTranslation();
     return (
         tags && (
             <footer className="w3-container">
+                <hr />
+                <p className="w3-text-grey">{t("keywords")}</p>
                 {tags.map((tag) => (
                     <Link
-                        className="w3-padding"
                         key={tag.id}
                         to={"/tags/" + tag.keyword.en}
                     >
-                        <h5 className="w3-tag w3-teal w3-round">
-                            {tag.keyword[i18n.language]}
+                        <h5 className="w3-tag w3-teal w3-round w3-small">
+                            {tag.keyword[i18n.language]??t('translation not found')}
                         </h5>
                     </Link>
                 ))}
