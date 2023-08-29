aa142a5c66347f0399a92553129746060b14588a
Yousef Fatouraee <fatouraee96@gmail.com>
2023-08-29 07:24:06 -0600

    Remove extar hr tag and add check for tag elements before rendering the component

--- resources/js/presentation/components/Tags.jsx
+++ resources/js/presentation/components/Tags.jsx
@@ -2,19 +2,17 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
 const Tags = ({ tags }) => {
-    const { i18n,t } = useTranslation();
+    const { i18n, t } = useTranslation();
+    console.log(tags);
     return (
-        tags && (
+        tags[0] && (
             <footer className="w3-container">
-                <hr />
                 <p className="w3-text-grey">{t("keywords")}</p>
                 {tags.map((tag) => (
-                    <Link
-                        key={tag.id}
-                        to={"/tags/" + tag.keyword.en}
-                    >
+                    <Link key={tag.id} to={"/tags/" + tag.keyword.en}>
                         <h5 className="w3-tag w3-teal w3-round w3-small">
-                            {tag.keyword[i18n.language]??t('translation not found')}
+                            {tag.keyword[i18n.language] ??
+                                t("translation not found")}
                         </h5>
                     </Link>
                 ))}
