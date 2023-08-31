3e857e30104906a54aecddb91ee1c6d9e1ed3679
Yousef Fatouraee <fatouraee96@gmail.com>
2023-08-31 02:12:32 -0600

    Add min width for the button

--- resources/js/presentation/components/MoreButton.jsx
+++ resources/js/presentation/components/MoreButton.jsx
@@ -8,7 +8,7 @@ export const moreButton = ({ to }) => {
             <Link
                 to={to}
                 className="w3-button w3-border w3-border-teal w3-hover-teal w3-text-teal"
-                style={{ borderRadius: "5px", width: "10%" }}
+                style={{ borderRadius: "5px", width: "10%", minWidth: "100px" }}
             >
                 {t("more")}
             </Link>
