ba08924621edf3be31fa5bf57fbf918a600d83a1
Yousef Fatouraee <fatouraee96@gmail.com>
2023-08-31 03:08:15 -0600

    Change loading component style to an overlay

--- resources/js/presentation/components/Loading.jsx
+++ resources/js/presentation/components/Loading.jsx
@@ -1,11 +1,15 @@
-export const Loading = () => (
-    <div className="w3-container w3-center w3-margin">
-        <i
-            className="fa fa-spinner w3-spin w3-text-teal"
-            style={{ fontSize: "64px" }}
-        ></i>
-        {/* <p>Loading...</p> */}
+import React from "react";
+
+const LoadingOverlay = () => (
+    <div className="loading-overlay">
+        <div className="loading-content">
+            <i
+                className="fa fa-spinner w3-spin w3-text-teal"
+                style={{ fontSize: "64px" }}
+            ></i>
+            {/* <p>Loading...</p> */}
+        </div>
     </div>
 );
 
-export default Loading;
+export default LoadingOverlay;
